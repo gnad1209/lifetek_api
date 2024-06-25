3eba0741a253db70205c0cf479dc3b838433014e
tiendang09012002 <tiendang09012002@gmail.com>
2024-06-25 15:35:09 +0700

    a

--- service/getToken.js
+++ service/getToken.js
@@ -3,7 +3,6 @@ const axios = require('axios')
 const qs = require('qs');
 const host = `https://identity.lifetek.vn`;
 const tokenEndpoint = `${host}:9443/oauth2/token`;
-const ROLE_VIEW_SCOPE = 'internal_role_mgt_view';
 
 const agent = new https.Agent({
     rejectUnauthorized: false,
