e1ef0965b6355eb7f795901ead03dd458dc67f64
ikyuss <danga120902@gmail.com>
2024-06-26 07:51:14 +0700

    abc

--- appConn.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-const uri = process.env.DATABASE_ROLE_DEPARTMENT || `mongodb+srv://t6ngl4m:uJxJWYn5rRjZrTEK@mxhtm0xx.vw82yvp.mongodb.net/db-iam?retryWrites=true&w=majority&appName=MxHTM0xx`
-
-module.exports = mongoose.createConnection(uri);
